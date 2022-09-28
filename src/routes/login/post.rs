9347641b4ae44f739f5a6a31ea7d026d77890006
startdusk <522383345@qq.com>
2022-09-28 23:28:09 +0800

    feat✨:  tracing login

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,9 @@
 use actix_web::http::header::LOCATION;
 use actix_web::{web, HttpResponse};
 use secrecy::Secret;
+use sqlx::PgPool;
+
+use crate::authentication::{validate_credentials, Credentials};
 
 #[derive(Debug, serde::Deserialize)]
 pub struct FormData {
@@ -8,6 +11,24 @@ pub struct FormData {
     password: Secret<String>,
 }
 
-pub async fn login(_form: web::Data<FormData>) -> HttpResponse {
-    HttpResponse::Ok().insert_header((LOCATION, "/")).finish()
+#[tracing::instrument(
+    skip(form, pool),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+    match validate_credentials(credentials, &pool).await {
+        Ok(user_id) => {
+            tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+            HttpResponse::SeeOther()
+                .insert_header((LOCATION, "/"))
+                .finish()
+        }
+        Err(_) => todo!(),
+    }
 }
